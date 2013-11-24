0baa6f57e8e703b30c923a94f8515a2aac73fb4f
Pascal Forster <paesc91@hotmail.com>
2013-11-24 16:28:23 +0100

    moved button

--- src/views/LoanDetail.java
+++ src/views/LoanDetail.java
@@ -329,7 +329,7 @@ public class LoanDetail extends JFrame {
 				}
 			});
 			
-			pnlCustomerLoans.add(btnSelektierteAusleiheAbschliessen,
+			pnlLoans.add(btnSelektierteAusleiheAbschliessen,
 					gbc_btnSelektierteAusleiheAbschliessen);
 
 			// ///////////////////////////////////////////////
